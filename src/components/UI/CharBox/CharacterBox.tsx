e52febd2794e59ace049bf0722b783f21f00ee1b
33jarek <33patryk.jarosz@gmail.com>
2026-06-01 17:15:22 +0200

    puh

--- src/components/UI/CharBox/CharacterBox.tsx
+++ src/components/UI/CharBox/CharacterBox.tsx
@@ -1,21 +1,32 @@
-import { killerList } from "../../../data/variables"
-
+import { killerList } from "../../../data/variables";
 
 type Props = {};
 
-export default function CharacterBox({ }: Props) {
-    return (
-        <div className="w-2/5">
-            {killerList.map(([name, info]) => (
-                <div key={name} className="relative h-200">
-                    <p>The {name}</p>
-                    <div className="">
-                        <img src="/Images/CharPortrait_bg.webp" alt="bg" className="absolute inset-0 w-full h-full object-cover" />
-                        <img src="/Images/CharPortrait_roleBG.webp" alt="rolebg" className="absolute inset-0 w-full h-full object-cover" />
-                        <img src={info.portrait} alt={name} className="absolute inset-0 w-full h-full object-contain z-100" />
-                    </div>
-                </div>
-            ))}
+export default function CharacterBox({}: Props) {
+  return (
+    <div className="w-2/5">
+      {killerList.map(([name, info]) => (
+        <div key={name} className="relative h-200">
+          <p>The {name}</p>
+          <div className="">
+            <img
+              src="/Images/CharPortrait_bg.webp"
+              alt="bg"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+            <img
+              src="/Images/CharPortrait_roleBG.webp"
+              alt="rolebg"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+            <img
+              src={info.portrait}
+              alt={name}
+              className="absolute inset-0 w-full h-full object-contain z-100"
+            />
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
--- src/routes/buildCreator.tsx
+++ src/routes/buildCreator.tsx
@@ -1,27 +1,49 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { killerList } from "../data/variables";
+import { killerList, killerUniversal, survivorList } from "../data/variables";
+import builds from "../data/builds.json";
 
 export const Route = createFileRoute("/buildCreator")({
   component: RouteComponent,
 });
 
+const killerBuilds = Object.entries(builds.killers);
 function RouteComponent() {
   return (
     <div className="flex flex-col">
       <h1 className="text-9xl">Build Creator</h1>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-1">
+        <div key={killerUniversal} className="flex w-full">
+          <div className="relative w-1/4 h-64">
+            <img
+              src="/Images/CharPortrait_bg.webp"
+              alt="bg"
+              className="absolute inset-0 w-full h-full object-contain z-0"
+            />
+            <img
+              src="/Images/CharPortrait_roleBG.webp"
+              alt="role bg"
+              className="absolute inset-0 w-full h-full object-contain z-10"
+            />
+            <img
+              src={killerUniversal}
+              alt={killerUniversal}
+              className="absolute inset-0 w-full h-full object-contain z-20"
+            />
+          </div>
+          <div className="h-64 w-3/4"></div>
+        </div>
         {killerList.map(([name, info]) => (
-          <div key={name} className="w-full">
-            <div className="relative w-64 h-64">
+          <div key={name} className="flex w-full">
+            <div className="relative w-1/4 h-64">
               <img
                 src="/Images/CharPortrait_bg.webp"
                 alt="bg"
-                className="absolute inset-0 w-full h-full object-cover z-0"
+                className="absolute inset-0 w-full h-full object-contain z-0"
               />
               <img
                 src="/Images/CharPortrait_roleBG.webp"
                 alt="role bg"
-                className="absolute inset-0 w-full h-full object-cover z-10"
+                className="absolute inset-0 w-full h-full object-contain z-10"
               />
               <img
                 src={info.portrait}
@@ -29,6 +51,36 @@ function RouteComponent() {
                 className="absolute inset-0 w-full h-full object-contain z-20"
               />
             </div>
+            <div className="h-64 w-3/4 ">
+              {killerBuilds.map(([characterName, BuildInfo]) => (
+                <div key={characterName} className="">
+                  {console.log(characterName, BuildInfo)}
+                  <h2>{B}</h2>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        {survivorList.map(([name, info]) => (
+          <div key={name} className="flex w-full">
+            <div className="relative w-1/4 h-64">
+              <img
+                src="/Images/CharPortrait_bg.webp"
+                alt="bg"
+                className="absolute inset-0 w-full h-full object-contain z-0"
+              />
+              <img
+                src="/Images/CharPortrait_roleBG.webp"
+                alt="role bg"
+                className="absolute inset-0 w-full h-full object-contain z-10"
+              />
+              <img
+                src={info.portrait}
+                alt={name}
+                className="absolute inset-0 w-full h-full object-contain z-20"
+              />
+            </div>
+            <div className="h-64 w-3/4"></div>
           </div>
         ))}
       </div>
