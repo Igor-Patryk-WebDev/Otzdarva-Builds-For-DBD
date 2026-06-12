0135514fcb7f3ad9fc801d3baecdfcbe39e0be3f
Pinkify_ <huelleigor@gmail.com>
2026-06-12 14:04:21 +0200

    New styles for front page role select buttons

--- src/components/pages/FrontPage/RoleSelectButton/RoleSelectButton.tsx
+++ src/components/pages/FrontPage/RoleSelectButton/RoleSelectButton.tsx
@@ -13,23 +13,27 @@ export const RoleSelectButton = ({ variant }: RoleSelectButtonProps) => {
   }
   const styles = {
     Killers: {
-      link: "bg-killer",
+      gradient: "killer",
+      link: "border-killer",
       img: "",
-      text: ""
+      text: "text-center",
+      flex: "flex-0"
     },
     Survivors: {
-      link: "bg-survivor",
+      gradient: "survivor",
+      link: "border-survivor",
       img: "ml-auto",
-      text: "right-4"
+      text: "text-center",
+      flex: "flex-1"
     }
   }
   return (
     <Link
       to={`/${variant.toLowerCase()}` as "/killers" | "/survivors"}
-      className={`${styles[variant]["link"]} block p-4 w-full relative`}
+      className={`${styles[variant]["link"]} border-2 bg-linear-to-t from-${styles[variant].gradient} to-neutral-900 rounded-md block px-4 py-4 h-64 w-full overflow-clip relative`}
       viewTransition={{ types: () => variant === "Killers" ? ["slide-right"] : ["slide-left"] }}>
-      <img src={images[variant]} alt={variant} className={`${styles[variant]['img']} h-64`} />
-      <h3 className={`${styles[variant]['text']} font-bold text-3xl absolute bottom-4`}>{variant.toUpperCase()}</h3>
+      <img src={images[variant]} alt={variant} className={`${styles[variant]['img']} absolute top-16 right-1/2 translate-x-1/2 h-64`} />
+      <h3 className={`${styles[variant]['text']} font-bold text-2xl`}>{variant.toUpperCase()}</h3>
     </Link>
   )
 }
