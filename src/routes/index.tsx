ea7cf8ff6fb93eee499079166f18b0013436b174
33jarek <33patryk.jarosz@gmail.com>
2026-06-16 15:52:23 +0200

    announcements pc layout with keybinds

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { RoleSelectWrapper } from "@components/pages/FrontPage";
 import { WebsiteBanner } from "@components/pages/FrontPage";
 import { LastUpdated } from "@components/pages/FrontPage";
+import { AnnouncementsPortal } from "@components/pages/FrontPage/Announcements/AnnouncementsPortal";
 import { ReportBugButton } from "@components/pages/FrontPage/ReportBugButton";
 import { SelfPlug } from "@components/pages/FrontPage/SelfPlug";
 import { SocialWrapper } from "@components/pages/FrontPage/SocialWrapper";
@@ -8,6 +9,8 @@ import { Button } from "@components/shared/Button";
 import { useHotkey } from "@tanstack/react-hotkeys";
 
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
 export const Route = createFileRoute("/")({
   component: RootPage,
@@ -16,25 +19,39 @@ export const Route = createFileRoute("/")({
 function RootPage() {
   const navigate = useNavigate();
 
+  const [showPortal, setShowPortal] = useState(false);
+
   useHotkey("Q", () =>
     navigate({ to: "/killers", viewTransition: { types: ["to-killers"] } }),
   );
   useHotkey("E", () =>
     navigate({ to: "/survivors", viewTransition: { types: ["to-survivors"] } }),
   );
+  useHotkey("N", () => setShowPortal(true));
+  useHotkey("Escape", () => setShowPortal(false));
 
   // useHotkeySequence(["S", "E", "C", "R", "E", "T"], () => console.log("hi"), { timeout: 1000 })
 
   return (
     <section
-      className="h-full flex center p-8"
+      className="h-full flex center p-8 relative"
       style={{ viewTransitionName: "front-page" }}
     >
       <div className="flex max-w-full flex-col center">
         <WebsiteBanner />
         <LastUpdated />
         <RoleSelectWrapper />
-        <Button className="p-2 rounded-lg absolute top-4 right-4">🔔</Button>
+        <Button
+          className="p-2 rounded-lg absolute top-4 right-4"
+          onClick={() => setShowPortal(true)}
+        >
+          🔔
+        </Button>
+        {showPortal &&
+          createPortal(
+            <AnnouncementsPortal onClose={() => setShowPortal(false)} />,
+            document.body,
+          )}
         <SocialWrapper />
 
         <SelfPlug />
